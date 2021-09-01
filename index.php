aaa088f250967c475b30bff8e2cb323740b5c770
Julio Canizalez <juliocanizalez@outlook.com>
2021-09-01 11:19:15 -0600

    :tada: Add initial index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./static/styles/styles.css" />
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <title>Tarea 3</title>
+</head>
+
+<body>
+  <main class="main">
+    <section class="main-title">
+      <h1>Manipulación de archivos con php</h1>
+    </section>
+    <section class="main-menu"></section>
+  </main>
+</body>
+
+</html>
